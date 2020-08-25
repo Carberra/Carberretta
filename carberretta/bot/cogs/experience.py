c1a23c2c13893dd288cb8df3a0013452d3037fc6
Max Shaughnessy <max@mshaugh.com>
2020-08-25 22:50:11 +0100

    Add experience and level functions

--- carberretta/bot/cogs/experience.py
+++ carberretta/bot/cogs/experience.py
@@ -6,6 +6,7 @@ Handles user experience stats:
     Level rewarding;
     Opt-in and -out commands.
 """
+import math
 
 import discord
 from discord.ext import commands
@@ -16,6 +17,15 @@ from carberretta import Config
 class Experience(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    async def level_to_experience(self, level: int) -> int:
+        return math.floor(sum(math.floor(x + 300 * 2 ** (x / 7.0)) for x in range(1, level)) / 4)
+
+    async def experience_to_level(self, experience: int) -> int:
+        level = 1
+        while await self.level_to_experience(level) < experience:
+            level += 1
+        return level
 
     @commands.Cog.listener()
     async def on_ready(self) -> None:
