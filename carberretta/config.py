4c82cb8aeadb5a782c8733d7051f36859c013533
Max Shaughnessy <max@mshaugh.com>
2020-08-25 23:21:49 +0100

    Add new configuration system

--- carberretta/config.py
+++ carberretta/config.py
@@ -1,33 +1,41 @@
-from typing import Final
-from os import getenv
+import typing
+from os import environ
+from pathlib import Path
 
-from dotenv import load_dotenv
-
-
-# Load the .env development environment if exists
-load_dotenv()
+import dotenv
 
 
-class Config:
-    # Production token is provided by a file, whereas the development token is
-    # given in .env.
-    try:
-        with open(getenv("TOKEN", "")) as f:
-            token = f.read()
-    except FileNotFoundError:
-        token = getenv("TOKEN", "")
+dotenv.load_dotenv()
 
-    TOKEN: Final = token
-    OWNER_IDS: Final[set] = set([385807530913169426, 102733198231863296])
-    PREFIX: Final = getenv("PREFIX", "+")
 
-    GUILD_ID: Final = int(getenv("GUILD_ID", ""))
-    STDOUT_ID: Final = int(getenv("STDOUT_ID", ""))
-    COMMANDS_ID: Final = int(getenv("COMMANDS_ID", ""))
-    RELAY_ID: Final = int(getenv("RELAY_ID", ""))
-    MODMAIL_ID: Final = int(getenv("MODMAIL_ID", ""))
+class ConfigMeta(type):
+    def resolve_value(cls, value: str):
+        _map: typing.Dict[str, typing.Callable] = {
+            "bool": bool,
+            "int": int,
+            "float": float,
+            "file": lambda x: Path(x).read_text(),
+            "str": str,
+            "set": lambda x: set([cls.resolve_value(e.strip()) for e in x.split(",")])
+        }
 
-    HUB_GUILD_ID: Final = int(getenv("HUB_GUILD_ID", GUILD_ID))
-    HUB_STDOUT_ID: Final = int(getenv("HUB_STDOUT_ID", STDOUT_ID))
-    HUB_COMMANDS_ID: Final = int(getenv("HUB_COMMANDS_ID", COMMANDS_ID))
-    HUB_RELAY_ID: Final = int(getenv("HUB_COMMANDS_ID", RELAY_ID))
+        return _map[(v := value.split(":", maxsplit=1))[0]](v[1])
+
+    def resolve_key(cls, key: str):
+        try:
+            return cls.resolve_key(environ[key])
+        except:
+            return cls.resolve_value(key)
+
+    def __getattr__(cls, name):
+        try:
+            return cls.resolve_key(name)
+        except KeyError:
+            raise AttributeError(f"{name} is not a key in config.") from None
+
+    def __getitem__(cls, name):
+        return cls.__getattr__(name)
+
+
+class Config(metaclass=ConfigMeta):
+    pass
