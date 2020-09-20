3144731dcf21280aba513ca736919b7dc11b37ef
parafoxia <parafoxia@carberra.xyz>
2020-09-20 23:59:05 +0100

    Added modmail cooldown
    Added modmail cooldown of 1 hr.
    Fixed permissions bug with nickname validator.

--- carberretta/bot/cogs/mod.py
+++ carberretta/bot/cogs/mod.py
@@ -10,18 +10,23 @@ Handles automatic mod systems:
 **Manual moderation is handled by S4, and thus is not included.**
 """
 
+import datetime as dt
 import string
+import typing as t
+from collections import defaultdict
 
 import discord
 from discord.ext import commands
 
 from carberretta import Config
+from carberretta.utils import chron
 from carberretta.utils.emoji import UNICODE_EMOJI
 
 
 class Mod(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.modmail_cooldown: defaultdict = defaultdict(dt.datetime.utcnow)
 
         self.nickname_whitelist = set(
             string.ascii_letters
@@ -32,28 +37,35 @@ class Mod(commands.Cog):
         )
 
     async def modmail(self, message: discord.Message) -> None:
-        if not 50 <= len(message.content) <= 1000:
-            await message.channel.send("Your message should be between 50 and 1,000 characters long.")
-
-        else:
-            member = self.bot.guild.get_member(message.author.id)
-
-            await self.modmail_channel.send(
-                embed=discord.Embed.from_dict(
-                    {
-                        "title": "Modmail",
-                        "color": member.colour.value,
-                        "thumbnail": {"url": f"{member.avatar_url}"},
-                        "footer": {"text": f"ID: {message.id}"},
-                        "image": {"url": att[0].url if len((att := message.attachments)) else None},
-                        "fields": [
-                            {"name": "Member", "value": member.mention, "inline": False},
-                            {"name": "Message", "value": message.content, "inline": False},
-                        ],
-                    }
-                )
+        if (retry_after := (self.modmail_cooldown[message.author.id] - dt.datetime.utcnow()).total_seconds()) > 0:
+            return await message.channel.send(
+                f"You're still on cooldown. Try again in {chron.long_delta(dt.timedelta(seconds=retry_after))}."
             )
-            await message.channel.send("Message sent. If needed, a moderator will DM you regarding this issue.")
+
+        if not 50 <= len(message.content) <= 1000:
+            return await message.channel.send("Your message should be between 50 and 1,000 characters long.")
+
+        member = self.bot.guild.get_member(message.author.id)
+
+        await self.modmail_channel.send(
+            embed=discord.Embed.from_dict(
+                {
+                    "title": "Modmail",
+                    "color": member.colour.value,
+                    "thumbnail": {"url": f"{member.avatar_url}"},
+                    "footer": {"text": f"ID: {message.id}"},
+                    "image": {"url": att[0].url if len((att := message.attachments)) else None},
+                    "fields": [
+                        {"name": "Member", "value": member.mention, "inline": False},
+                        {"name": "Message", "value": message.content, "inline": False},
+                    ],
+                }
+            )
+        )
+        await message.channel.send(
+            "Message sent. If needed, a moderator will DM you regarding this issue. You'll need to wait 1 hour before sending another modmail."
+        )
+        self.modmail_cooldown[message.author.id] = dt.datetime.utcnow() + dt.timedelta(seconds=3600)
 
     async def unhoist(self, nickname: str) -> str:
         while nickname and nickname[0] not in string.ascii_letters:
@@ -83,30 +95,34 @@ class Mod(commands.Cog):
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member) -> None:
         if before.nick != after.nick and after.nick:
-            nickname = await self.unhoist("".join(c for c in after.nick if c in self.nickname_whitelist))
+            try:
+                nickname = await self.unhoist("".join(c for c in after.nick if c in self.nickname_whitelist))
 
-            if await self.nickname_valid(nickname):
-                if nickname != after.nick:
-                    await after.edit(nick=nickname, reason="Nickname contains invalid characters")
-            else:
-                await after.edit(
-                    nick=before.nick if await self.nickname_valid(before.nick) else None, reason="Invalid nickname"
-                )
+                if await self.nickname_valid(nickname):
+                    if nickname != after.nick:
+                        await after.edit(nick=nickname, reason="Nickname contains invalid characters")
+                else:
+                    await after.edit(
+                        nick=before.nick if await self.nickname_valid(before.nick) else None, reason="Invalid nickname"
+                    )
+            except discord.Forbidden:
+                pass
 
     @commands.command(name="validatenicknames", aliases=["va"])
     @commands.has_permissions(manage_nicknames=True)
     async def validatenicknames_command(self, ctx):
         for member in ctx.guild.members:
             if member.nick:
-                nickname = await self.unhoist("".join(c for c in member.nick if c in self.nickname_whitelist))
+                try:
+                    nickname = await self.unhoist("".join(c for c in member.nick if c in self.nickname_whitelist))
 
-                if await self.nickname_valid(nickname):
-                    if nickname != member.nick:
-                        await member.edit(nick=nickname, reason="Nickname contains invalid characters")
-                else:
-                    await member.edit(
-                        nick=None, reason="Invalid nickname"
-                    )
+                    if await self.nickname_valid(nickname):
+                        if nickname != member.nick:
+                            await member.edit(nick=nickname, reason="Nickname contains invalid characters")
+                    else:
+                        await member.edit(nick=None, reason="Invalid nickname")
+                except discord.Forbidden:
+                    pass
         await ctx.send("Done.")
 
 
