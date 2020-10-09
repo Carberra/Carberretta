cd37adecdad92249943131d5e99065fc9f2e592f
parafoxia <parafoxia@carberra.xyz>
2020-10-09 03:45:14 +0100

    Updated emoji systems
    Added `EmojiGetter` class to the `emoji` util.
    Carberretta can now use the emojis in the hub guild.

--- carberretta/utils/emoji.py
+++ carberretta/utils/emoji.py
@@ -1,3 +1,5 @@
+from discord import utils
+
 UNICODE_EMOJI = [
     "\U0001F947",
     "\U0001F948",
@@ -3864,3 +3866,63 @@ UNICODE_EMOJI = [
     "\U0001F9DF",
     "\U0001F4A4",
 ]
+
+# If Carberretta can't use the correct emojis, fall back to these.
+ALTERNATIVES = {
+    "confirm": "✅",
+    "cancel": "❎",
+    "exit": "⏏️",
+    "info": "ℹ️",
+    "loading": "⏱️",
+    "option1": "1️⃣",
+    "option2": "2⃣",
+    "option3": "3⃣",
+    "option4": "4⃣",
+    "option5": "5⃣",
+    "option6": "6⃣",
+    "option7": "7⃣",
+    "option8": "8⃣",
+    "option9": "9⃣",
+    "pageback": "◀️",
+    "pagenext": "▶️",
+    "stepback": "⏪",
+    "stepnext": "⏩",
+}
+
+
+class EmojiGetter:
+    def __init__(self, bot):
+        self.bot = bot
+
+    def get(self, name):
+        hub = self.bot.get_cog("Hub")
+
+        if getattr(hub, "guild", None) is not None:
+            return utils.get(hub.guild.emojis, name=name) or ALTERNATIVES[name]
+        else:
+            return ALTERNATIVES[name]
+
+    def get_many(self, *names):
+        hub = self.bot.get_cog("Hub")
+
+        if getattr(hub, "guild", None) is not None:
+            return [utils.get(hub.guild.emojis, name=name) or ALTERNATIVES[name] for name in names]
+        else:
+            return [ALTERNATIVES[name]]
+
+    def yield_many(self, *names):
+        hub = self.bot.get_cog("Hub")
+
+        if getattr(hub, "guild", None) is not None:
+            for name in names:
+                yield utils.get(hub.guild.emojis, name=name) or ALTERNATIVES[name]
+        else:
+            yield ALTERNATIVES[name]
+
+    def mention(self, name):
+        hub = self.bot.get_cog("Hub")
+
+        if getattr(hub, "guild", None) is not None:
+            return f"{utils.get(hub.guild.emojis, name=name) or ALTERNATIVES[name] or ''}"
+        else:
+            return ALTERNATIVES[name] or ""
