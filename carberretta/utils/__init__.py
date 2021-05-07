c5dbe7be88c67bc4a5f489bb8d9a9a05099ac156
Lord Farquaad <73174454+IkBenOlie5@users.noreply.github.com>
2021-05-07 15:02:41 +0200

    Delete carberretta directory

--- carberretta/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).parent.parent.parent
-DEFAULT_EMBED_COLOUR = 0xE33939
-
-# Dependant on above constants.
-from .emoji import EmojiGetter
-from .loc import CodeCounter
-from .ready import Ready
