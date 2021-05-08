d595bf510ace1a2cb42ee3ea7e5c78517a90cf12
Jonx <jon@jonxtech.com>
2021-05-07 18:58:39 -0700

    optimize database calls in feeds cog
    update command function names
    add space for formatting error
    
    update to store values in db as proper type
     - changed db schema
     - made corresponding changes to feeds.py

--- carberretta/data/static/build.sql
+++ carberretta/data/static/build.sql
@@ -1,19 +1,25 @@
-CREATE TABLE IF NOT EXISTS feeds (
-    ContentType text NOT NULL PRIMARY KEY,
-    ContentValue text DEFAULT ""
+CREATE TABLE IF NOT EXISTS videos (
+    ID int NOT NULL PRIMARY KEY,
+    Vid text DEFAULT "",
+    Vod text DEFAULT ""
 );
 
-INSERT OR IGNORE INTO feeds (ContentType, ContentValue)
-VALUES
-    ("video", ""),
-    ("vod", ""),
-    ("stream_start", ""),
-    ("stream_end", ""),
-    ("stream_live", "0"),
-    ("stream_message", "");
+INSERT OR IGNORE INTO videos (ID)
+VALUES (1);
 
 CREATE TABLE IF NOT EXISTS premieres (
     VideoID text NOT NULL PRIMARY KEY,
     Upcoming int,
     Announced int
 );
+
+CREATE TABLE IF NOT EXISTS streams (
+    ID int NOT NULL PRIMARY KEY,
+    StreamStart text DEFAULT "",
+    StreamEnd text DEFAULT "",
+    StreamLive int DEFAULT 0,
+    StreamMessage int DEFAULT 0
+);
+
+INSERT OR IGNORE INTO streams (ID)
+VALUES (1);
