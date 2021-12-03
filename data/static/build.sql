458b0ffe024b3ac49dc8c2d24b98891942d404fe
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-12-03 11:06:48 +0000

    Update SQL file to add tables for support cases

--- data/static/build.sql
+++ data/static/build.sql
@@ -11,3 +11,26 @@ CREATE TABLE IF NOT EXISTS errors (
     err_cmd TEXT,
     err_text TEXT
 );
+
+CREATE TABLE IF NOT EXISTS support_cases (
+    case_id TEXT PRIMARY KEY,
+    client_id INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS support_instances (
+    case_id TEXT,
+    instance_id TEXT,
+    channel_id INTEGER NOT NULL,
+    message_id INTEGER NOT NULL,
+    opened_at NUMERIC DEFAULT CURRENT_TIMESTAMP,
+    closed_at NUMERIC,
+    PRIMARY KEY (case_id, instance_id),
+    FOREIGN KEY (case_id) REFERENCES support_cases(case_id)
+);
+
+CREATE TABLE IF NOT EXISTS support_history (
+    case_id TEXT PRIMARY KEY,
+    author_id INTEGER NOT NULL,
+    is_helper INTEGER NOT NULL,
+    FOREIGN KEY (case_id) REFERENCES support_cases(case_id)
+);
