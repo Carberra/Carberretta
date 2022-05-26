ed76b93c0abc6b7a2642eb9d08af4a1b3f985e09
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-05-26 15:07:55 +0100

    Update license years

--- carberretta/utils/__init__.py
+++ carberretta/utils/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2021, Carberra Tutorials
+# Copyright (c) 2020-present, Carberra Tutorials
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
