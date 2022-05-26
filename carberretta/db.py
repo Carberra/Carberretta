5142c8ae9b93f1ec3b92948dbec9b8c1a5b696b3
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-05-26 19:36:51 +0100

    Fix typing issue

--- carberretta/db.py
+++ carberretta/db.py
@@ -65,7 +65,9 @@ class RowData(dict[str, t.Any]):
         raise ValueError("row data cannot be modified")
 
     @classmethod
-    def from_selection(cls, cur: aiosqlite.Cursor, row: aiosqlite.Row) -> RowData:
+    def from_selection(
+        cls, cur: aiosqlite.Cursor, row: aiosqlite.Row[int | float | str]
+    ) -> RowData:
         def _resolve(field: int | float | str) -> t.Any:
             if isinstance(field, (int, float)) or not STRFTIME_PATTERN.match(field):
                 return field
