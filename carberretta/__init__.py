e65935c2f46948a07de269f4ce851e2ac9dac727
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-09-23 01:43:34 +0100

    Change version number to something Discord actually likes reee

--- carberretta/__init__.py
+++ carberretta/__init__.py
@@ -36,7 +36,7 @@ from .config import Config
 from .db import Database
 
 __productname__ = "Carberretta"
-__version__ = "2.0.0α"
+__version__ = "2.0.0a"
 __description__ = "A Discord bot designed for the Carberra Discord server."
 __url__ = "https://github.com/Carberra/Carberretta"
 __authors__ = ("Ethan Henderson", "Max Shaughnessy")
