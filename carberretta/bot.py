85bd9cdc9d70bc876ca685878c5f36ad74e089fb
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-09-27 00:07:22 +0100

    Disable banner

--- carberretta/bot.py
+++ carberretta/bot.py
@@ -51,6 +51,7 @@ bot = lightbulb.BotApp(
     owner_ids=Config.OWNER_IDS,
     case_insensitive_prefix_commands=True,
     intents=hikari.Intents.ALL,
+    banner=None,
 )
 bot.d._dynamic = Path("./data/dynamic")
 bot.d._static = bot.d._dynamic.parent / "static"
