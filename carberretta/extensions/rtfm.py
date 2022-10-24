5ab968ea0eaeffa1e336151ab5ce0d31993f3070
Yuito | 碧 唯翔 <aoi.yuito.ehou@gmail.com>
2022-10-25 01:07:16 +0600

    forgot to add the ephemeral for the rtfm refresh command

--- carberretta/extensions/rtfm.py
+++ carberretta/extensions/rtfm.py
@@ -153,7 +153,7 @@ async def python_rtfm(ctx: lightbulb.SlashContext) -> None:
 @rtfm_group.child
 @lightbulb.add_checks(lightbulb.owner_only)
 @lightbulb.command(
-    "refresh", description="Refreshes the rtfm caches.", auto_defer=True
+    "refresh", description="Refreshes the rtfm caches.", auto_defer=True, ephemeral=True
 )
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def refresh_rtfm(ctx: lightbulb.SlashContext) -> None:
